97d141cedc052afd3dd419162fdfd17ac3d74484
@quytruong <quytruong@var-meta.com>
2024-12-14 01:41:05 +0700

    feat: change to SSG

--- src/lib/getStatic.ts
+++ src/lib/getStatic.ts
@@ -1,4 +1,4 @@
-import { type GetServerSidePropsContext, type PreviewData } from 'next';
+import { type GetServerSidePropsContext, type GetStaticPropsContext, type PreviewData } from 'next';
 import { i18n } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { type ParsedUrlQuery } from 'querystring';
@@ -17,9 +17,7 @@ export const getStaticPaths = () => ({
   paths: getI18nPaths(),
 });
 
-export const getI18nProps = async (ctx: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>, ns = ['common']) => {
-  console.log(ctx.locale, 'jsdljkfljksdfjkl');
-
+export const getI18nProps = async (ctx: GetStaticPropsContext<ParsedUrlQuery>, ns = ['common']) => {
   const locale = ctx.locale || 'vi';
   const props = {
     ...(await serverSideTranslations(locale, ns)),
@@ -30,6 +28,18 @@ export const getI18nProps = async (ctx: GetServerSidePropsContext<ParsedUrlQuery
 export const makeServerSideProps =
   (ns: Array<nsType> = ['common', 'zod']) =>
   async (ctx: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>) => {
+    if (process.env.NODE_ENV === 'development') {
+      await i18n?.reloadResources();
+    }
+
+    return {
+      props: await getI18nProps(ctx, ns),
+    };
+  };
+
+export const makeStaticProps =
+  (ns: Array<nsType> = ['common', 'zod']) =>
+  async (ctx: GetStaticPropsContext<ParsedUrlQuery>) => {
     if (process.env.NODE_ENV === 'development') {
       await i18n?.reloadResources();
     }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { makeServerSideProps } from '@/lib/getStatic';
+import { makeStaticProps } from '@/lib/getStatic';
 import LandingPage from '@/modules/LandingPage';
 
 export default LandingPage;
 
-const getServerSideProps = makeServerSideProps();
-export { getServerSideProps };
+const getStaticProps = makeStaticProps();
+export { getStaticProps };
